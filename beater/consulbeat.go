61cf1002847aa0a693ebf819528a3b78106ee2c3
Stefan Caraiman <stefanc.caraiman@gmail.com>
2018-12-08 00:32:31 +0200

    Add further healthcheck logic

--- beater/consulbeat.go
+++ beater/consulbeat.go
@@ -52,8 +52,7 @@ func (bt *Consulbeat) Run(b *beat.Beat) error {
 	}
 
 	ticker := time.NewTicker(bt.config.Period)
-	//health := consulClient.Health()
-	//agent :=  consulClient.Agent()
+	health := consulClient.Health()
 	catalog := consulClient.Catalog()
 
 	for {
@@ -62,38 +61,18 @@ func (bt *Consulbeat) Run(b *beat.Beat) error {
 			return nil
 		case <-ticker.C:
 		}
-		// Get Node and Service checks
-		// Use Catalog
-		// nodes, meta, err := catalog.Nodes(nil)
-		// if err != nil {
-		// 	panic(err)
-		// }
 
-		// if meta.LastIndex == 0 {
-		// 	fmt.Printf("Bad: %v", meta)
-		// }
-
-		// if len(nodes) == 0 {
-		// 	fmt.Printf("Bad: %v", nodes)
-		// }
-		// fmt.Println("Nodes: ", nodes)
-		// for key, value := range nodes {
-		// 	fmt.Printf("Key[%s] value [%s] \n", key, value)
-		// }
-		// fmt.Println(nodes[0].Datacenter)
-		// fmt.Println(nodes[0].Address)
-		// services
 		services, meta_services, err := catalog.Services(nil)
 		if err != nil {
 			panic(err)
 		}
 
 		if meta_services.LastIndex == 0 {
-			fmt.Printf("Bad: %v", meta_services)
+			logp.Info("Bad: %v", meta_services)
 		}
 
 		if len(services) == 0 {
-			fmt.Printf("Bad: %v", services)
+			logp.Info("Bad: %v", services)
 		}
 
 		var events = []beat.Event{}
@@ -105,39 +84,36 @@ func (bt *Consulbeat) Run(b *beat.Beat) error {
 			}
 
 			if meta.LastIndex == 0 {
-				fmt.Printf("Bad: %v", meta)
+				logp.Info("Bad: %v", meta)
 			}
 
 			if len(checks) == 0 {
-				fmt.Printf("Bad: %v", checks)
+				logp.Info("Bad: %v", checks)
 			}
 
-			fmt.Println("Service name is: ", service)
-			fmt.Println("Node status: ", checks[0].Node)
-			for _, check_value := range checks {
-				fmt.Println("Checked value is ", check_value)
-				fmt.Println(check_value.ServiceAddress)
-				fmt.Println(check_value.Checks)
-				event := beat.Event{
-					Timestamp: time.Now(),
-					Fields: common.MapStr{
-						"node":         check_value.Node,
-						"Datacenter":   check_value.Datacenter,
-						"AgentAddress": check_value.Address,
-						// "CheckID":        health_check_def.CheckID,
-						// "CheckName":      health_check_def.Name,
-						// "Status":         health_check_def.Status,
-						// "Output":         health_check_def.Output,
-						"ServiceID":      check_value.ServiceID,
-						"ServicePort":    check_value.ServicePort,
-						"ServiceAddress": check_value.ServiceAddress,
-						"ServiceName":    check_value.ServiceName,
-						"ServiceTags":    strings.Join(check_value.ServiceTags, ","),
-					},
+			for _, service_check := range checks {
+				health_service_check, _, _ := health.Checks(service_check.ServiceName, nil)
+				for _, check := range health_service_check {
+					event := beat.Event{
+						Timestamp: time.Now(),
+						Fields: common.MapStr{
+							"Node":           service_check.Node,
+							"Datacenter":     service_check.Datacenter,
+							"AgentAddress":   service_check.Address,
+							"CheckID":        check.CheckID,
+							"CheckName":      check.Name,
+							"Status":         check.Status,
+							"Output":         check.Output,
+							"ServiceName":    check.ServiceName,
+							"ServiceTags":    strings.Join(check.ServiceTags, ","),
+							"ServiceID":      service_check.ServiceID,
+							"ServicePort":    service_check.ServicePort,
+							"ServiceAddress": service_check.ServiceAddress,
+						},
+					}
+					events = append(events, event)
 				}
-				events = append(events, event)
 			}
-
 		}
 		bt.client.PublishAll(events)
 		logp.Info("Events sent")
